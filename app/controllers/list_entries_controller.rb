68318e9ae8e8fda4281bd6515aef2701dd6d764d
Sameer Siruguri <siruguri@gmail.com>
2014-07-28 11:03:15 -0700

    developer form

--- app/controllers/list_entries_controller.rb
+++ app/controllers/list_entries_controller.rb
@@ -5,15 +5,22 @@ class ListEntriesController < ApplicationController
 
     email = params[:emailaddress]
     comment = params[:subscriber_comments]
-    puts ">>> #{params}"
+    app_link = params[:app_link]
 
+    if params[:list_id]
+      list_id = params[:list_id]
+    else
+      list_id = ENV['mc_general_list_id']
+    end
+    
+    puts ">>> #{params}"
     mc = Mailchimp::API.new(ENV['MAILCHIMP_API_KEY'])
     l=ListEntry.create email: email
     @mc_error=''
 
     # Hard-coding default list on LYN Co. account (login id = lynco_ss)
     begin
-      mc.lists.subscribe('00b1e06338', {email: email}, {'subcomment' => comment}, 'html', false)
+      mc.lists.subscribe(list_id, {email: email}, {'subcomment' => comment, 'applink' => app_link}, 'html', false)
     rescue Mailchimp::ListAlreadySubscribedError
       @mc_error = "#{email} is already subscribed to the list."
     rescue Mailchimp::ListDoesNotExistError
